9ee376a42d351af0776c73f2aed226d63c3df343
fanghg2018 <fanghg2018@gmail.com>
2025-09-26 17:15:27 +0800

    9.26

--- src/main/puppeteer-controller.js
+++ src/main/puppeteer-controller.js
@@ -3,25 +3,31 @@ const puppeteer = require('puppeteer')
 let browser = null
 
 async function runPuppeteerTask(url, logFn = console.log) {
-  logFn(`启动 Puppeteer，打开网址: ${url}`)
+  if (browser !== null) {
+    // 如果浏览器已经启动，直接返回
+    logFn('Puppeteer 已经在运行中。')
+    return
+  } else {
+    logFn(`启动 Puppeteer，打开网址: ${url}`)
 
-  browser = await puppeteer.launch({
-    headless: false, // 如果需要无头浏览器，设置为 true
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
-  })
+    browser = await puppeteer.launch({
+      headless: false, // 如果需要无头浏览器，设置为 true
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
+    })
 
-  const page = await browser.newPage()
-  page.on('console', (msg) => {
-    logFn(msg.text()) // 捕获 Puppeteer 的 console log
-  })
+    const page = await browser.newPage()
+    page.on('console', (msg) => {
+      logFn(msg.text()) // 捕获 Puppeteer 的 console log
+    })
 
-  await page.goto(url, { waitUntil: 'domcontentloaded' })
+    await page.goto(url, { waitUntil: 'domcontentloaded' })
 
-  logFn(`页面加载完成：${url}`)
+    logFn(`页面加载完成：${url}`)
 
-  const title = await page.title()
+    const title = await page.title()
 
-  return title //
+    return title //
+  }
 }
 
 async function stopAutomation() {
